ff3a82343644b18d423f22f79e27f9c43a551570
daisuke0207 <daisuke02074869@gmail.com>
2020-12-16 20:44:38 +0900

    postデータベース作成

--- db/migrate/20201216031145_create_posts.rb
+++ db/migrate/20201216031145_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string :title, null: false
+      t.text :body, null: false
+      t.boolean :is_publish, null: false
+      t.references :user, foreign_key: true, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_070356) do
+ActiveRecord::Schema.define(version: 2020_12_16_031145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.boolean "is_publish", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nickname", null: false
@@ -28,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_12_15_070356) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
