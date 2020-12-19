64f61248cdda2a9108cfbb02cdca8bd8b62a65b9
daisuke0207 <daisuke02074869@gmail.com>
2020-12-19 14:56:52 +0900

    createアクションの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :is_publish).merge(user_id: current_user.id)
+  end
 end
