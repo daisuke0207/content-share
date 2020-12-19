55a367184f62551ecfc168d564a574648e809f55
daisuke0207 <daisuke02074869@gmail.com>
2020-12-19 16:32:31 +0900

    post_controller#index : すべての投稿データ取得
    orderメソッドのcreated_at DESCで新しい投稿順に一覧表示させる。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @posts = Post.includes(:user).order('created_at DESC')
+  end
 
   def new
     @post = Post.new
