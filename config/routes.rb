a30e31fc1c1bf2a70a78a856823be27e023a98d5
daisuke0207 <daisuke02074869@gmail.com>
2020-12-23 16:04:08 +0900

    commnets controller削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:index, :create]
+  end
   resources :users, only: :show do
     collection do
       get 'my_post'
