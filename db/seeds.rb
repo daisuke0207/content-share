1642ce90fb489eb41581b67d022b4bb3c46bf18e
daisuke0207 <daisuke02074869@gmail.com>
2020-12-29 09:13:14 +0900

    test data seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |n|
+  Post.create!(
+      title: "whiteの#{n+1}番目のタイトルです。whiteの#{n+1}番目のタイトルです。",
+      body: "#{n+1}番目のコンテンツです。#{n+1}番目のコンテンツです。#{n+1}番目のコンテンツです。#{n+1}番目のコンテンツです。#{n+1}番目のコンテンツです。",
+      is_publish: true,
+      user_id: 1
+  )
+end
+
+50.times do |n|
+  Post.create!(
+      title: "blackの#{n+1}番目のタイトルです。blackの#{n+1}番目のタイトルです。",
+      body: "#{n+1}番目のコンテンツです。#{n+1}番目のコンテンツです。#{n+1}番目のコンテンツです。#{n+1}番目のコンテンツです。#{n+1}番目のコンテンツです。",
+      is_publish: true,
+      user_id: 2
+  )
+end
